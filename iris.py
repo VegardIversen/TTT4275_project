b6e336656e086141fb8611843be173776417e7ae
VegardIversen <vegard-si@hotmail.com>
2021-03-24 16:02:07 +0100

    changed iris.csv to get columnnames

--- iris.py
+++ iris.py
@@ -12,15 +12,15 @@ path = './Iris_TTT4275/iris.csv'
 #---------Importing data------------#
 def load_data(path, d=','):
     data = pd.read_csv(path, sep=',') #reading csv file, and splitting with ","
-    data.columns = ['sepal_length','sepal_width','petal_length','petal_width','species']#making columnnames, for easier understanding
+    #data.columns = ['sepal_length','sepal_width','petal_length','petal_width','species']#making columnnames, for easier understanding
     #data.describe()#this gives all the information you need: count, mean, std, min, 25%, 50%,75%,max
     return data
 
 #--------^Importing data^-----------#
 data = load_data(path)
+print(data)
 #---------Preprocessing-------------#
-#train, test = train_test_split(data, test_size = 0.4, stratify = data["species"], random_state = 42)# example for splitting dataset
+#train, test = train_test_split(data, test_size = 20/50, stratify = data["species"], random_state = 42)# example for splitting dataset, this somewhat wrong for now
 #--------^Preprocessing^------------#
 
 
-print(data.species)
