ea5cb5826be3f3b97620e8f3d16b616214d67caa
Christian D. Nguyen <Christng@stud.ntnu.no>
2021-04-20 14:11:46 +0200

    parprogrammering digitalt fungerer ikke

--- Iris_TTT4275/task1.py
+++ Iris_TTT4275/task1.py
@@ -18,23 +18,41 @@ def load_data(path, d=','):
     return data
 #--------^Importing data^-----------#
 
-#---------------Math----------------#
-def sigmoid(x):
-    return 1/(1+np.exp(-x))
-#--------------^Math^---------------#
-
 #----------Preprocessing------------#
 data = load_data(path)
 
 #---------^Preprocessing^-----------#
 
 #------------Processing-------------#
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+def accumulateMSE(gk, tk)
+    return 0.5*np.matmul((gk-tk).T, (gk-tk))
+
+def get_gk(gk, W, xk)
+### Equation 20
+    zk = np.matmul(W, xk)
+    return sigmoid(zk)
+
+def get_grad_gk_MSE(gk, tk)
+    return gk - tk
+
+def get_grad_zk_g(gk)
+    return np.matmul(gk, (1-gk))
+
+def get_grad_W_zk(xk, features)
+    return xk.reshape(1, features + 1)
+
+def accumulate_grad_W_MSE(x, y, z)
+    return np.matmul(np.multiply(x, y), z)
+
 def train(data, iterations, alpha, classes, features):
 
     MSE_list = []
 
     gk = np.zeros(classes)
-    gk[0] = 1                                   #?
+    gk[0] = 1                                 #?
     tk = np.zeros((classes, 1))
     W  = np.zeros((classes, (features+1)))
 
@@ -44,63 +62,25 @@ def train(data, iterations, alpha, classes, features):
         MSE = 0
 
         for xk in data:
-
-            #Eq. 20
-            zk = np.matmul(W, xk)
-            gk = sigmoid()                          #Nani
+            #matmul = normal multiplication, #multiply = element-wise multiplication
+            xk.reshape(features + 1, 1)
+            gk = findgk(W, xk)
 
             #Eq. 22
-            grad_gk_MSE = gk - tk
-            grad_zk_g   = np.matmul(gk, (1-gk))
-            grad_W_zk   = xk.reshape(features, 1)   #WHAT IS THE SHAPE OF XK?
+            grad_gk_MSE = get_grad_gk_MSE(gk, tk)
+            grad_zk_g   = get_grad_zk_g(gk)
+            grad_W_zk   = get_grad_W_zk(xk, features)   #xk is the shape of 5x1 |x1k| which transposes to |x1k x2k x3k x4k 1|
+                                                        #                       |x2k|
+                                                        #                       |x3k|
+                                                        #                       |x4k|
+                                                        #                       |1  |
 
-            grad_W_MSE += 
+            grad_W_MSE += accumulate_grad_W_MSE(grad_gk_MSE, grad_zk_g, grad_W_zk)
         
             #Eq. 19
-            MSE += 0.5*np.multiply((gk-tk).reshape(), (gk-tk))
+            MSE += accumulateMSE(gk, tk)
 
     MSE_list.append(MSE)
-
-
-
-    #g = Wx
-    #From eq. 20
-    # MSE_list = []
-
-    # gk = np.zeros(C)       
-    # gk[0] = 1              #
-    # tk = np.zeros((C, 1))  #The target variable of a dataset is the feature of a dataset about which you want to gain a deeper understanding
-    # W = np.zeros((C, 1))  #
-
-    # for i in range(iterations):
-
-    #     grad_W_MSE = 0
-    #     MSE = 0
-
-    #     for xk in data:
-
-    #         #Eq. 20: zk = W*xk
-
-
-    #         gk = sigmoid(np.matmul(W,xk))
-
-    #         tk = 
-
-    #         # Eq. 22
-    #         grad_gk_MSE = np.multiply((gk-tk), gk) #(gk_t_k)*g_k
-    #         grad_W_zk = xk.reshape() #x_k is an array, we want it to be reshaped into 
-
-    #         #np.multiply(x1,x2,..,xN) element-wise multiplication
-    #         #np.matmul(x1,x2,...,xN) 
-    #         grad_W_MSE += np.matmul(np.multiply(grad_gk_MSE, 1-gk), grad_W_zk) #Eq. 22
-
-    #         MSE += 0.5*np.matmul(gk-tk).T, (gk-tk))
-    #     MSE_list.append(MSE[0])
-
-
-    # # Eq. 23
-    # W = W - alpha*grad_W_MSE 
-    # return W
 
 #-----------^Processing^------------#
 print(len(train()[0]))
