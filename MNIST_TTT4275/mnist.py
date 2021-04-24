0903fbd9f87baf0e2ae81f5b7c9e01413987bc7f
Christian D. Nguyen <Christng@stud.ntnu.no>
2021-04-24 17:29:33 +0200

    Changing task from Vowels to MNIST. Loaded the MNIST dataset and plotted some images

---
+++ MNIST_TTT4275/mnist.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io
+from keras.datasets import mnist
+
+(train_X, train_y), (test_X, test_y) = mnist.load_data()
+
+print('X_train: ' + str(train_X.shape))
+print('Y_train: ' + str(train_y.shape))
+print('X_test:  '  + str(test_X.shape))
+print('Y_test:  '  + str(test_y.shape))
+
+
+
+for i in range(9):  
+    plt.subplot(330 + 1 + i)
+    plt.imshow(train_X[i], cmap=plt.get_cmap('gray'))
+
+plt.show()
